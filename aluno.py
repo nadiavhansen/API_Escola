3152868fd161a75fe843b7409b06bf0141ee78f6
nadiavhansen <nadiavhansen@gmail.com>
2021-04-29 17:16:36 -0300

    atualizacao 3

--- aluno.py
+++ aluno.py
@@ -4,6 +4,7 @@ import json
 from conexo_mongo import *
 import pandas as pd
 from random import randint
+from bson.objectid import ObjectId
 
 class Aluno:
 
@@ -13,4 +14,31 @@ class Aluno:
     def realizar_prova(self, dict_values):
         a = Mongodb()
         a.alunos_prova.insert_one(dict_values)
+
+    def conferir_matricula(self, codigo_matricula):
+        a = Mongodb()
+        alunos = a.alunos_matricula.find({"Matricula": codigo_matricula}, {'_id': 0, 'Matricula': 1}).count()
+        if alunos > 0:
+            return True
+        return False
+
+    def chamar_gabarito(self, id_prova):
+        a = Mongodb()
+
+        respostas = a.provas.find_one({"_id": ObjectId(id_prova)}, {"_id": 0})
+        for numero_questao in respostas["Questoes"]:
+            del respostas["Questoes"][numero_questao]["Alternativas"]
+            del respostas["Questoes"][numero_questao]["Peso"]
+        return respostas
+
+
+a = Aluno()
+b = a.chamar_gabarito("608ae511a4478812bd4e0561")
+print(b)
+questoes = b['Questoes']
+print(questoes)
+print(questoes)
+
+
+
 
--- rotas_alunos.py
+++ rotas_alunos.py
@@ -10,19 +10,11 @@ from bson.objectid import ObjectId
 app = Flask(__name__)
 
 
-def conferir_matricula(codigo_matricula):
-    a = Mongodb()
-    alunos = a.alunos_matricula.find({"Matricula": codigo_matricula}, {'_id': 0, 'Matricula': 1}).count()
-    if alunos > 0:
-        return True
-    return False
-
-
 @app.route("/listar_provas/<matricula>", methods=["GET"])
 def listar_provas(matricula):
     a = Mongodb().provas
     try:
-        if conferir_matricula(matricula):
+        if Aluno().conferir_matricula(matricula):
             df = pd.DataFrame(a.find({}, {"_id": 1, "Nome": 1}))
             df = df.astype(str)
             json = df.to_json(orient="records")
@@ -39,7 +31,7 @@ def acessar_provas(matricula, id_prova):
     a = Mongodb().provas
 
     try:
-        if conferir_matricula(matricula):
+        if Aluno().conferir_matricula(matricula):
             prova = a.find_one({"_id": ObjectId(id_prova)}, {"_id": 0})
             for numero_questao in prova["Questoes"]:
                 del prova["Questoes"][numero_questao]["Resposta correta"]
@@ -55,11 +47,9 @@ def acessar_provas(matricula, id_prova):
 def realizar_prova(matricula, id_prova):
     raw_request = request.data.decode("utf-8")
     dict_values = json.loads(raw_request)
-    dict_values['Matricula'] = matricula
-    dict_values['id_prova'] = id_prova
 
     try:
-        if conferir_matricula(matricula):
+        if Aluno().conferir_matricula(matricula):
             a = Aluno()
             a.realizar_prova(dict_values)
             return "Prova realizada com sucesso!", 200
@@ -83,6 +73,26 @@ if __name__ == "__main__":
 #         for numero_questao in prova["Questoes"]:
 #             del prova["Questoes"][numero_questao]["Resposta correta"]
 #         return prova
+#
+#     except Exception as error:
+#         return str(error.args)
+
+#
+#
+# @app.route("/realizar_prova/<matricula>/<id_prova>", methods=["POST"])
+# def realizar_prova(matricula, id_prova):
+#     raw_request = request.data.decode("utf-8")
+#     dict_values = json.loads(raw_request)
+#     dict_values['Matricula'] = matricula
+#     dict_values['id_prova'] = id_prova
+#
+#     try:
+#         if Aluno().conferir_matricula(matricula):
+#             a = Aluno()
+#             a.realizar_prova(dict_values)
+#             return "Prova realizada com sucesso!", 200
+#         else:
+#             return "Matrícula não existe!"
 #
 #     except Exception as error:
 #         return str(error.args)
